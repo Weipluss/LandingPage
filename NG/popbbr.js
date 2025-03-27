0ebfd50b3d9812f545b4446c1f1c7967e8764cb2
Weipluss <q917162074@gmail.com>
2025-03-27 21:52:48 +0800

    1

--- NG/popbbr.js
+++ NG/popbbr.js
@@ -1,5 +1,5 @@
 var binomclickid = new URLSearchParams(window.location.search).get('clickid') || '';
-var baseUrl = 'https://m.bolo2vas61.click/c/n/258335/4989';
+var baseUrl = 'https://m.bolo2vas68.click/c/n/270009/4989';
 var under = back = baseUrl + (binomclickid ? '?cid=' + encodeURIComponent(binomclickid) : '');
 console.log('binomclickid value:', binomclickid);
 console.log('under value:', under);
